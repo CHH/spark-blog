65a10533348df26885374c7e896848dde4276b56
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2013-02-24 19:49:50 +0100

    Adapt class names to new Spark\ActionPack package

--- config/application.php
+++ config/application.php
@@ -5,7 +5,12 @@ namespace Blog;
 use Doctrine\Common\Cache\ApcCache;
 
 $app['spark.app.name'] = 'Blog';
-# $app['spark.view_context_class'] = '\MyApp\ViewContext';
+
+# Override the default class which is used as View Context, default is 
+# "$AppName\ViewContext"
+# $app['spark.action_pack.view_context_class'] = '\MyApp\ViewContext';
+
+$app['monolog.logfile'] = '/tmp/spark-app.log';
 
 # All your controller and model classes are loaded from this namespaces. By
 # default its set to your app name.
@@ -13,13 +18,13 @@ $app['spark.app.name'] = 'Blog';
 # $app['spark.default_module'] = "MyApp";
 
 # This is the base class which should be extended by all your controllers.
-class ApplicationController extends \Spark\Controller\Base
+class ApplicationController extends \Spark\ActionPack\Controller\Base
 {
     # Include Action Helper traits here to make them available
     # in all your controllers.
 }
 
-class ViewContext extends \Spark\Controller\ViewContext
+class ViewContext extends \Spark\ActionPack\ViewContext
 {
     # Include your custom view helpers here.
 }
